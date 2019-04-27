987a4fa7d8c261ecc0bfb98467dbf1aae9de622a
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-04-27 22:29:37 +0530

    Substring query!

--- string_functions/substring.sql
+++ string_functions/substring.sql
@@ -1,6 +1,9 @@
 SELECT
     book_id,
-    title,
+    CONCAT(
+        SUBSTRING(title, 1, 20), 
+        "..."
+    ) AS short_title,
     CONCAT(author_fname, "" ,author_lname) AS author_fullname,
     released_year,
     stock_quantity,
